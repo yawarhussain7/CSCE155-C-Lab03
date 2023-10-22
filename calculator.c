1aa7b5ca45b05f635d155294bb59c59338c880c8
= <=>
2023-10-22 02:29:35 -0700

    Remove function and simple use if else for option 6

--- calculator.c
+++ calculator.c
@@ -10,17 +10,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-int min(int a,int b)
-{
-  if(a > b)
-  {
-    return a;
-  }
-  else
-  return b;
-}
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
   double a, b, result;
   int choice;
@@ -40,37 +32,57 @@ int main(int argc, char **argv) {
   printf("(6) log_a(b)\n");
   scanf("%d", &choice);
 
-  if(choice == 1) {
+  if (choice == 1)
+  {
     printf("%f", a + b);
-  } else if(choice == 2) {
+  }
+  else if (choice == 2)
+  {
     result = a - b;
     printf("%f", result);
-  } else if(choice == 3) {
-    //TODO: handle this case (multiplication)
+  }
+  else if (choice == 3)
+  {
+    // TODO: handle this case (multiplication)
     result = a * b;
-    printf("%f",result);
-  } else if(choice == 4) {
-    //TODO: handle this case (division)
+    printf("%f", result);
+  }
+  else if (choice == 4)
+  {
+    // TODO: handle this case (division)
 
-    if(b>0)
+    if (b > 0)
     {
-      result = a/b;
-      printf("%f", a /b);
+      result = a / b;
+      printf("%f", a / b);
     }
     else
     {
       printf("ERROR divisor is less then 1 \n");
     }
-
-  } else if(choice == 5) {
-    //TODO: handle this case (minimum)
-    result = min(a,b);
-    printf("%f",result);
-  } else if(choice == 6) {
-    //TODO: handle this case (log_a(b))
-      result =  log(b)/log(a);
-      printf("%f",result);
-  } else {
+  }
+  else if (choice == 5)
+  {
+    // TODO: handle this case (minimum)
+    if (a > b)
+    {
+      result = b;
+      printf("%f", result);
+    }
+    else
+    {
+      result = a;
+      printf("%f", result);
+    }
+  }
+  else if (choice == 6)
+  {
+    // TODO: handle this case (log_a(b))
+    result = log(b) / log(a);
+    printf("%f", result);
+  }
+  else
+  {
     printf("Please input a valid operator next time");
   }
 
